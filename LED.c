19559e4bce3cf745f99a82412301df7512493ce3
jahan0 <48150802+jahan0@users.noreply.github.com>
2019-03-08 09:57:58 +0530

    Add files via upload

---
+++ LED.c
@@ -0,0 +1,17 @@
+#include<pic.h>
+void delay(unsigned int x);
+void main()
+{
+TRISB=0x00;
+while(1)
+{
+PORTB=0xFF;
+delay(6500);
+PORTB=0x00;
+delay(6500);
+}
+}
+void delay(unsigned int x)
+{
+while(x--);
+}
